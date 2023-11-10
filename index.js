63703bf8495203570764a7b83333c79e8efdd984
Rohitsenji <rohitsenji1234@gmail.com>
2023-11-10 21:31:38 +0530

    Added Details

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const jsondata = require('./Package')
 const server = http.createServer((req,res)=>{
 
     if(req.url==='/'){
-        res.write("<h1>Hello, Welcome Home</h1>")
+        res.write("<title>Home page of Handson-1</title><h1>Hello, Welcome Home</h1>")
+        res.write("<h1>What is Node?</h1>")
+        res.write("<p>Node.js is an open-source, cross-platform JavaScript runtime environment built on Chrome's V8 JavaScript engine. It allows developers to execute JavaScript code outside of a web browser, enabling server-side development. Node.js is event-driven and non-blocking, making it efficient for handling concurrent operations. It has a large ecosystem of libraries and frameworks, making it popular for building scalable and high-performance web applications, APIs, and server-side applications.</p>")
+        res.write("<h1>Beneifits Of Node</h1>")
+        res.write("<ol><li>JavaScript Everywhere</li><li>Fast Execution</li><li>Asynchronous and Non-blocking</li></ol>")
         res.end()
     }
     else if(req.url === '/product'){
@@ -15,6 +19,7 @@ const server = http.createServer((req,res)=>{
     }
     else if(req.url === '/contact'){
         res.write("<h1>Contact us on instagram roony_rohit</h1>")
+        
         res.end()
     }
     else{
